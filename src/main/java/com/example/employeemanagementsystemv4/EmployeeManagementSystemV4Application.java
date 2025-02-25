84fecf60cb393595626d024b1f429ce3feb431f8
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2025-02-25 14:02:50 +0300

    Initial commit

---
+++ src/main/java/com/example/employeemanagementsystemv4/EmployeeManagementSystemV4Application.java
@@ -0,0 +1,13 @@
+package com.example.employeemanagementsystemv4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemV4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementSystemV4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/employeemanagementsystemv4/EmployeeManagementSystemV4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeemanagementsystemv4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemV4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
